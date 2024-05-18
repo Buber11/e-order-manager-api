df916af5b1be2502c52ee6328b03328cc30354fb
Buber11 <105680119+Buber11@users.noreply.github.com>
2024-05-18 19:28:51 +0200

    fix client

--- src/main/java/com/example/eordermanagerapi/Client/Client.java
+++ src/main/java/com/example/eordermanagerapi/Client/Client.java
@@ -2,13 +2,19 @@ package com.example.eordermanagerapi.Client;
 
 import com.example.eordermanagerapi.user.User;
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
 import lombok.Builder;
-import lombok.CustomLog;
 import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.Date;
+
 
 @Entity
 @Builder
 @Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class Client {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -18,6 +24,6 @@ public class Client {
     @JoinColumn(name = "user_id", referencedColumnName = "id_user")
     private User user;
     @Column(name = "sing_up_date")
-    private Data signUpDate;
+    private Date signUpDate;
 
 }
